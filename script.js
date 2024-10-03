1aee0e89b3bda82ff839deeefd87b0eae6895311
Sjur <sjursaa@gmail.com>
2024-10-03 21:36:49 +0200

    add button for adjusting gridsize

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+let gridSize = 16;
 // Task 1: Create squares 16x16 divs inside container:
 const container = document.querySelector("#container");
 container.setAttribute("style", "display: flex; flex-direction: column");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < gridSize; i++) {
   const divContainer = document.createElement("div-container");
   divContainer.classList.add("container2");
   divContainer.setAttribute("style", "display: flex; flex-direction: row");
   container.appendChild(divContainer);
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < gridSize; i++) {
     const div = document.createElement("div");
     div.classList.add("div");
     div.textContent = "div";
@@ -20,6 +21,11 @@ for (let i = 0; i < 16; i++) {
 }
 
 // TODO: add button to create new fresh grid, get user input to decide number of squares (userInput1xuserInput1)
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  gridSize = prompt("plz type grid value");
+  console.log(gridSize);
+});
 
 // TODO: Extra! Randomize rgb color for each hover
 
