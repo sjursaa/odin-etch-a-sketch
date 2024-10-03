d48e95cead979415f5b7b64ceb4a060b5defdb67
Sjur <sjursaa@gmail.com>
2024-10-03 18:38:38 +0200

    change container from class to id

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("hello, world");
+
+// TODO: create squares 16x16 divs inside container:
+
+// TODO: setup hover event listener to change color of divs
+//
+// TODO: add button to create new fresh grid, get user input to decide number of squares (userInput1xuserInput1)
+//
+// TODO: Extra! Randomize rgb color for each hover
+
+// TODO: Extra! Implement progressive darkening 10% for hover (CSS Opacity)
