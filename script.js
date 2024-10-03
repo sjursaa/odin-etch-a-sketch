31456c5329b5fb0aa0c996e42e55096081dd0cf0
Sjur <sjursaa@gmail.com>
2024-10-03 19:59:35 +0200

    create full 16x16 grid with divs containing the word div

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-console.log("hello, world");
-
-// TODO: create squares 16x16 divs inside container:
+// Task 1: Create squares 16x16 divs inside container:
 const container = document.querySelector("#container");
 container.setAttribute("style", "display: flex; flex-direction: column");
 
 for (let i = 0; i < 16; i++) {
-  const container2 = document.createElement("container2");
-  container2.classList.add("container2");
-  container2.setAttribute("style", "display: flex; flex-direction: row");
-  container.appendChild(container2);
+  const divContainer = document.createElement("div-container");
+  divContainer.classList.add("container2");
+  divContainer.setAttribute("style", "display: flex; flex-direction: row");
+  container.appendChild(divContainer);
   for (let i = 0; i < 16; i++) {
     const div = document.createElement("div");
     div.classList.add("div");
     div.textContent = "div";
     div.setAttribute("style", "color: blue; background: lightblue;");
-    container2.appendChild(div);
+    divContainer.appendChild(div);
   }
 }
 
 // TODO: setup hover event listener to change color of divs
-//
+
 // TODO: add button to create new fresh grid, get user input to decide number of squares (userInput1xuserInput1)
-//
+
 // TODO: Extra! Randomize rgb color for each hover
 
 // TODO: Extra! Implement progressive darkening 10% for hover (CSS Opacity)
