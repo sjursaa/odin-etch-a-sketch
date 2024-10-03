06e687adc45637f54e6109fa2fa682f709be4f7b
Sjur <sjursaa@gmail.com>
2024-10-03 21:39:06 +0200

    add function for drawing grid, call function when button is pressed

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 let gridSize = 16;
 // Task 1: Create squares 16x16 divs inside container:
 const container = document.querySelector("#container");
-container.setAttribute("style", "display: flex; flex-direction: column");
 
-for (let i = 0; i < gridSize; i++) {
-  const divContainer = document.createElement("div-container");
-  divContainer.classList.add("container2");
-  divContainer.setAttribute("style", "display: flex; flex-direction: row");
-  container.appendChild(divContainer);
+function drawGrid() {
+  container.setAttribute("style", "display: flex; flex-direction: column");
+
   for (let i = 0; i < gridSize; i++) {
-    const div = document.createElement("div");
-    div.classList.add("div");
-    div.textContent = "div";
-    div.setAttribute("style", "color: blue; background: lightblue;");
-    divContainer.appendChild(div);
-    div.addEventListener("mouseover", () => {
-      div.setAttribute("style", "background: red;");
-    });
+    const divContainer = document.createElement("div-container");
+    divContainer.classList.add("container2");
+    divContainer.setAttribute("style", "display: flex; flex-direction: row");
+    container.appendChild(divContainer);
+    for (let i = 0; i < gridSize; i++) {
+      const div = document.createElement("div");
+      div.classList.add("div");
+      div.textContent = "div";
+      div.setAttribute("style", "color: blue; background: lightblue;");
+      divContainer.appendChild(div);
+      div.addEventListener("mouseover", () => {
+        div.setAttribute("style", "background: red;");
+      });
+    }
   }
 }
 
@@ -25,7 +28,10 @@ const button = document.querySelector("button");
 button.addEventListener("click", () => {
   gridSize = prompt("plz type grid value");
   console.log(gridSize);
+  drawGrid();
 });
+
+drawGrid();
 
 // TODO: Extra! Randomize rgb color for each hover
 
