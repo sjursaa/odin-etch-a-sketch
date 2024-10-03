072172d00f769fa3d5c54f24f86c76f1d07232a7
Sjur <sjursaa@gmail.com>
2024-10-03 19:01:42 +0200

    create 16 divs containing the letter c, arranged horizontally

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 console.log("hello, world");
 
 // TODO: create squares 16x16 divs inside container:
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+  const div = document.createElement("div");
+  div.classList.add("row");
+  div.textContent = "c";
+  div.setAttribute(
+    "style",
+    "color: blue; background: lightblue; display: flex; flex-direcition: row",
+  );
+  container.appendChild(div);
+}
 
 // TODO: setup hover event listener to change color of divs
 //
